6c67ce9d2c85dfe00d2bc6c51079408158029af1
Kevin Brown <brownkm5@g.cofc.edu>
2016-10-03 13:21:42 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){ //this makes it so that jquery doesnt load, even if the script tags are at the top, until the html has loaded
+'use strict';
+
+var heading = jQuery('.heading');
+
+console.log(heading);
+
+
+});
+}(jQuery));
