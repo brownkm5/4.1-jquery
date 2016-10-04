fc73dcb834218f952f000a922243413887817d2a
Kevin Brown <brownkm5@g.cofc.edu>
2016-10-03 21:06:16 -0400

    finished jquery functions, not a lot of styling

--- main.js
+++ main.js
@@ -2,10 +2,25 @@
   $(function(){ //this makes it so that jquery doesnt load, even if the script tags are at the top, until the html has loaded
 'use strict';
 
-var heading = jQuery('.heading');
+$(".drop-down-one").hide();
+$(".drop-down-two").hide();
+$(".drop-down-three").hide();
 
-console.log(heading);
 
+$(".files").click(function() {
+  $(".drop-down-one").slideToggle( 200, function() {
+  });
+});
+
+$(".mail").click(function() {
+  $(".drop-down-two").slideToggle( 200, function() {
+  });
+});
+
+$(".cloud").click(function() {
+  $(".drop-down-three").slideToggle( 200, function() {
+  });
+});
 
 });
 }(jQuery));
