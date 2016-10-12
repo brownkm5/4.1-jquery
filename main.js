f1696f5b4445fe1024864688109269158372843e
Kevin Brown <brownkm5@g.cofc.edu>
2016-10-12 15:53:05 -0400

    added accordion functction

--- main.js
+++ main.js
@@ -2,25 +2,28 @@
   $(function(){ //this makes it so that jquery doesnt load, even if the script tags are at the top, until the html has loaded
 'use strict';
 
-$(".drop-down-one").hide();
-$(".drop-down-two").hide();
-$(".drop-down-three").hide();
+$(".closed").hide();
 
+  $('.list').click(function(){
+    if($(this).next().is(':visible')){
+      $(this).next().slideUp(200, function(){
 
-$(".files").click(function() {
-  $(".drop-down-one").slideToggle( 200, function() {
+      });
+    } else {
+      $(this).next().slideDown(200, function(){
+
+      });
+      $(this).siblings('.list').next().slideUp(200, function(){
+
+      });
+    }
   });
-});
 
-$(".mail").click(function() {
-  $(".drop-down-two").slideToggle( 200, function() {
-  });
-});
 
-$(".cloud").click(function() {
-  $(".drop-down-three").slideToggle( 200, function() {
+  $(this).next().slideToggle(200, function(){
+
   });
-});
+
 
 });
 }(jQuery));
